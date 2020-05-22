622a63c6b8cd53722a22cb39e6893a8a43142155
Karan Kumar <karan.n.j.kumar@gmail.com>
2020-05-22 12:05:56 +0100

    fix return result

--- src/main/java/com/karankumar/bookproject/ui/shelf/DoubleToRatingScaleConverter.java
+++ src/main/java/com/karankumar/bookproject/ui/shelf/DoubleToRatingScaleConverter.java
@@ -71,7 +71,7 @@ public class DoubleToRatingScaleConverter implements Converter<Double, RatingSca
         } else if (ratingVal == 9.5) {
             return Result.ok(RatingScale.NINE_POINT_FIVE);
         } else if (ratingVal == 10.0) {
-            return Result.ok(RatingScale.ZERO);
+            return Result.ok(RatingScale.TEN);
         } else {
             return Result.error("Invalid rating");
         }
